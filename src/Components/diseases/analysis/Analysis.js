99d5aaca1cb530edca2ff2c4ab69451b68e8e26b
Beasr1 <d.ishaan001@gmail.com>
2022-07-27 20:50:34 +0530

    changed graph

--- src/Components/diseases/analysis/Analysis.js
+++ src/Components/diseases/analysis/Analysis.js
@@ -4,19 +4,20 @@ import img2 from './bar2.png'
 //import './analysisStyles.css'
 import styles from './Anal.module.css'
 import Graph from './Graph'
+import Graph2 from './Graph2'
 
 
 export const Analysis = () => {
   return (
     <div className={`${styles.container_analysis}`}>
-      
         <div className={`${styles.analysis_card}`}>
 
             <h2 className={`${styles.heading_analysis}`}>Analysis</h2>
             <div className={`${styles.pics_container}`}>
-            <img className={`${styles.pic_right}`}  src={img2} alt='pic_right' />
-                <Graph/>
-                {/* <img className={`${styles.pic_graph}`} src={img1} alt='pic_graph' /> */}
+              <Graph2/>
+              <img className={`${styles.pic_right}`}  src={img2} alt='pic_right' />
+              
+              {/* <img className={`${styles.pic_graph}`} src={img1} alt='pic_graph' /> */}
             </div>
 
         </div>
